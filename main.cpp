82a8e3a6aba0b3faa0ecc02bd142438ca564148a
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-09-17 13:15:56 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// prints a table out i guess
+
+#include <iostream>
+
+int main() {
+    // print table
+    std::cout << "| Courses      | Students     |" << std::endl;
+    std::cout << "|-----------------------------|" << std::endl;
+    std::cout << "| C++          |           50 |" << std::endl;
+    std::cout << "| JavaScript   |          100 |" << std::endl;
+
+    return 0;
+}
